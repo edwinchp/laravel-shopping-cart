e2152ed9dc598ad2c88a632d9f96869ec8f9a151
Edwin Chi Pineiro <chipineiro@gmail.com>
2020-08-10 23:36:02 -0500

    group rutes adding a prefix to them

--- routes/web.php
+++ routes/web.php
@@ -18,33 +18,35 @@ Route::get('/', [
     'as' => 'product.index'
 ]);
 
-Route::get('/signup', [
-    'uses' => 'UserController@getSignup',
-    'as' => 'user.signup'
-]);
 
-Route::post('/signup', [
-    'uses' => 'UserController@postSignup',
-    'as' => 'user.signup'
-]);
+Route::group(['prefix' => 'user'], function(){
 
-
-Route::get('/signin', [
-    'uses' => 'UserController@getSignin',
-    'as' => 'user.signin'
-]);
-
-Route::post('/signin', [
-    'uses' => 'UserController@postSignin',
-    'as' => 'user.signin'
-]);
-
-Route::get('user/profile', [
-    'uses' => 'UserController@getProfile',
-    'as' => 'user.profile'
-]);
-
-Route::get('/user/logout', [
-    'uses' => 'UserController@getLogout',
-    'as' => 'user.logout'
-]);
+    Route::get('/signup', [
+        'uses' => 'UserController@getSignup',
+        'as' => 'user.signup'
+    ]);
+    Route::post('/signup', [
+        'uses' => 'UserController@postSignup',
+        'as' => 'user.signup'
+    ]);
+    
+    Route::get('/signin', [
+        'uses' => 'UserController@getSignin',
+        'as' => 'user.signin'
+    ]);
+    
+    Route::post('/signin', [
+        'uses' => 'UserController@postSignin',
+        'as' => 'user.signin'
+    ]);
+    
+    Route::get('/profile', [
+        'uses' => 'UserController@getProfile',
+        'as' => 'user.profile'
+    ]);
+    
+    Route::get('/logout', [
+        'uses' => 'UserController@getLogout',
+        'as' => 'user.logout'
+    ]);
+});
